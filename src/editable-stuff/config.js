a3ac375061d2333a9f626d189a77b37d0693b02d
ekelly55 <eamonn.michael.kellyr@email.com>
2022-11-01 07:03:40 -0400

    change metadata

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -133,16 +133,8 @@ const experiences = {
   show: false,
   heading: "Experiences",
   data: [
-    {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
-    },
-    {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
-    },
+    {},
+    {},
   ]
 }
 
